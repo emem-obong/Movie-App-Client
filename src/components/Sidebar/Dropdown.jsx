7b8ced234f9ae8463d2c4eed691e00d310ec0499
unknown <emem-obong>
2023-10-26 15:38:16 +0100

    update sidebar css

--- src/components/Sidebar/Dropdown.jsx
+++ src/components/Sidebar/Dropdown.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
-import useAuth from '../../hooks/UseAuth'
-import { Link } from 'react-router-dom'
-import "./Sidebar.css"
+import React from "react";
+import useAuth from "../../hooks/UseAuth";
+import { Link } from "react-router-dom";
+import "./sidebar.css";
 
 const Dropdown = () => {
-
-    const {token, handleLogOutUser}= useAuth()
+  const { token, handleLogOutUser } = useAuth();
 
   return (
-    <div className='position-absolute  mt-3 my-dropdown'>
-        {token ?(
-            <div>
-            <button onClick={()=>{
-                handleLogOutUser()
+    <div className="position-absolute  mt-3 my-dropdown">
+      {token ? (
+        <div>
+          <button
+            onClick={() => {
+              handleLogOutUser();
             }}
-             className='btn btn-danger text-white'>LogOut</button>
+            className="btn btn-danger text-white"
+          >
+            LogOut
+          </button>
         </div>
-        ) :(
-<div className='d-flex flex-column gap-2'>
-            <Link to='/signin' className='btn btn-danger text-white'>SignIn</Link>
-            <Link to='/signup' className='btn btn-danger text-white'>SignUp</Link>
+      ) : (
+        <div className="d-flex flex-column gap-2">
+          <Link to="/signin" className="btn btn-danger text-white">
+            SignIn
+          </Link>
+          <Link to="/signup" className="btn btn-danger text-white">
+            SignUp
+          </Link>
         </div>
-        )}
-        
-        
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Dropdown
+export default Dropdown;
